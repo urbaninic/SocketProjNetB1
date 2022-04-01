918ef94418a682c6bf648a3bdc947df1fb2c89db
nicou <urbaninico2003@gmail.com>
2022-04-01 15:20:58 +0200

    project uplaod

---
+++ src/gestionesocket/ClientSocket.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionesocket;
+import java.net.*;
+
+
+/**
+ *
+ * @author nicou
+ */
+public class ClientSocket {
+    
+    ServerSocket serverSocket = new ServerSocket(2000);
+}
